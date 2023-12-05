6f2877a2ab5ece1d86fbdfee74ca25bf59613010
Russell Warner <rewarner@russellbits.com>
2023-12-05 15:12:28 -0500

    Version 0.1a — ready for static build.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import '$lib/styles/house.css';
+</script>
+
+<main>
+	<div class="logo">
+		<img src="images/nicolle-alien.png" width="100" height="100" alt="Alien smiley face" />
+	</div>
+	<div>
+		<img src="images/bio-picture.png" width="400" height="400" alt="Nicolle Bradford Avatar" />
+	</div>
+	<h1>Nicolle Bradford Plays</h1>
+	<p>Hype Instagator</p>
+</main>
+
+<style>
+	main {
+		color: #ff41ce;
+		text-align: center;
+		margin: auto;
+	}
+	.logo {
+		position: absolute;
+		top: 30px;
+		left: 30px;
+	}
+</style>
