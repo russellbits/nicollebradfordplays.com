a1e20988a17d2e12c9f7e5efdf3bc323d137dd93
Russell Warner <rewarner@russellbits.com>
2023-12-05 17:54:47 -0500

    Tets: checking component inclusion for static builds.

---
+++ src/lib/components/SocialButton.svelte
@@ -0,0 +1,6 @@
+<script>
+</script>
+
+<a href="https://pin.it/aYDLBjK"
+	><!--<i class="glyph-pinterest glyph-white" />-->My Mama Said (Pinterest)</a
+>
---
+++ src/lib/components/SocialLinks.svelte
@@ -0,0 +1,62 @@
+<script>
+	import '$lib/styles/entypo.css';
+</script>
+
+<ul>
+	<li>
+		<a href="https://pin.it/aYDLBjK"
+			><!--<i class="glyph-pinterest glyph-white" />-->My Mama Said (Pinterest)</a
+		>
+	</li>
+	<li>
+		<a href="https://www.facebook.com/nicollebradfordplays"
+			><!--<i class="glyph-facebook glyph-white" />-->Facebook</a
+		>
+	</li>
+	<li class="active">
+		<a href="https://instagram.com/mymammasaidshop"
+			><!--<i class="glyph-instagram glyph-white" />-->Instagram</a
+		>
+	</li>
+</ul>
+
+<style>
+	ul {
+		list-style-type: none;
+		margin: auto;
+		padding: 0;
+		width: 100%;
+		border: 1px solid red;
+	}
+	li {
+		float: left;
+		margin-left: 1em;
+		/* display: inline; */
+		color: #7f017f;
+		text-align: center;
+		padding: 14px 16px;
+		text-decoration: none;
+		background-color: #ffec00;
+		border: 2px solid #7f017f;
+		border-radius: 25px;
+	}
+	li a {
+		font-family: 'ChunkFive', sans-serif;
+		text-decoration: none;
+	}
+	li a:hover {
+		background-color: #f208d9;
+	}
+
+	.active {
+		background-color: #ffec00;
+	}
+
+	li {
+		/* border-right: 1px solid #bbb; */
+	}
+
+	li:last-child {
+		/* border-right: none; */
+	}
+</style>
