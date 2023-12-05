bd7aefce9c800c544ea8fe57d5ff70282f9dca1c
Russell Warner <rewarner@russellbits.com>
2023-12-05 17:53:48 -0500

    Generalized CSS.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,32 @@
 <script>
-	import '$lib/styles/house.css';
+	import '$lib/styles/house.scss';
+	// import SocialLinks from '$lib/components/SocialLinks.svelte';
 </script>
 
 <main>
 	<div class="logo">
 		<img src="images/nicolle-alien.png" width="100" height="100" alt="Alien smiley face" />
 	</div>
-	<div>
+	<div class="avatar">
 		<img src="images/bio-picture.png" width="400" height="400" alt="Nicolle Bradford Avatar" />
 	</div>
 	<h1>Nicolle Bradford Plays</h1>
-	<p>Hype Instagator</p>
+	<h2>Hype Instagator</h2>
+	<p>
+		<!-- <SocialLinks /> -->
+		<a href="https://pin.it/aYDLBjK">Pinterest</a> |
+		<a href="https://www.facebook.com/nicollebradfordplays"
+			><!--<i class="glyph-facebook glyph-white" />-->Facebook</a
+		>
+		|
+		<a href="https://instagram.com/mymammasaidshop"
+			><!--<i class="glyph-instagram glyph-white" />-->Instagram</a
+		>
+	</p>
 </main>
 
 <style>
 	main {
-		color: #ff41ce;
 		text-align: center;
 		margin: auto;
 	}
@@ -23,5 +34,8 @@
 		position: absolute;
 		top: 30px;
 		left: 30px;
+	}
+	.avatar {
+		padding: 1em;
 	}
 </style>
